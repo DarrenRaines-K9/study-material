07eb82be63de375f6e75b64a83647dc8da1b4c04
Darren Raines <draines.123123@gmail.com>
2025-07-09 14:14:46 -0500

    all course material for implentation entered into respective file

---
+++ Arrays/adding-items-to-arrays.js
@@ -0,0 +1,8 @@
+const clay = [ "Chunk of clay", "Chunk of clay", "Chunk of clay", "Chunk of clay" ]
+const toFireInKiln = []
+
+for () {
+   const mug = "coffee mug"
+}
+
+console.log(toFireInKiln)
---
+++ Arrays/array-iteration.js
@@ -0,0 +1,5 @@
+const topics = [  ]
+
+for () {
+
+}
